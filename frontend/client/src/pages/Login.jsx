87f7362768cab3803e1b262de5a4375217a5f841
Aakanshi Malik <155739971+aakanshimalik@users.noreply.github.com>
2025-04-10 01:02:42 +0530

    Update Login.jsx

--- frontend/client/src/pages/Login.jsx
+++ frontend/client/src/pages/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/login",
+        "https://zerodhaclone-ofq0.onrender.com/login",
         {
           ...inputValue,
         },
